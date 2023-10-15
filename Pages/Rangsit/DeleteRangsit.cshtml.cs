f79c48975ffce766fd488da15687c990445774e6
pee42 <pee42@REDSHIELD>
2023-10-16 02:51:45 +0700

    Add project files.

---
+++ Pages/Rangsit/DeleteRangsit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace inventorySystem.Pages.Rangsit
+{
+    public class DeleteRangsitModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Siam/IndexSiam.cshtml.cs
@@ -0,0 +1,46 @@
+using inventorySystem.Pages.Rangsit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+namespace inventorySystem.Pages.Siam
+{
+    public class IndexSiamModel : PageModel
+    {
+        public List<StockInfo> listStocks = new List<StockInfo>();
+        public void OnGet()
+        {
+            try
+            {
+                String connectionString = "Server=tcp:kullapat.database.windows.net,1433;Initial Catalog=inventory;Persist Security Info=False;User ID=sayoko;Password=Lol_5555;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM stocks WHERE storeid=2";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                StockInfo stockInfo = new StockInfo();
+                                stockInfo.itemid = "" + reader.GetInt32(0);
+                                stockInfo.item = reader.GetString(1);
+                                stockInfo.storeid = reader.GetString(2);
+                                stockInfo.supplier = reader.GetString(3);
+                                stockInfo.amount = reader.GetString(4);
+                                stockInfo.create_at = reader.GetDateTime(5).ToString();
+
+                                listStocks.Add(stockInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exceplion: " + ex.ToString());
+            }
+        }
+    }
+}
